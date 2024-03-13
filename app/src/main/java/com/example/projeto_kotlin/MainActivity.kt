1ae9e387e6cc29acaccaf480707f3fdf70b61dcb
Eduardo Aquiles Franco da Silva <86538404+eduardodev001@users.noreply.github.com>
2024-03-12 21:39:11 -0300

    Adição de botão de alteração de texto!!!

--- app/src/main/java/com/example/projeto_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/projeto_kotlin/MainActivity.kt
@@ -5,14 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.projeto_kotlin.ui.theme.Projeto_kotlinTheme
-import androidx.compose.material3.Text
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.sp
@@ -23,41 +26,45 @@ class MainActivity : ComponentActivity() {
         setContent {
             Projeto_kotlinTheme {
 
-                val h3 = TextStyle(
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 30.sp,
-                    letterSpacing = 0.15.sp
-                )
+
 
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    desenha_tela()
 
-                    Box(modifier = Modifier.fillMaxSize()) {
-
-                        Image(painter = painterResource(id = R.drawable.img1), contentDescription = null, modifier = Modifier.align(Alignment.TopStart).size(100.dp))
-
-                        Image(painter = painterResource(id = R.drawable.img2), contentDescription = null, modifier = Modifier.align(Alignment.TopEnd).size(100.dp))
-
-                        Image(painter = painterResource(id = R.drawable.img3), contentDescription = null, modifier = Modifier.align(Alignment.BottomStart).size(100.dp))
-
-                        Image(painter = painterResource(id = R.drawable.img4), contentDescription = null, modifier = Modifier.align(Alignment.BottomEnd).size(100.dp))
-
-                        Text(
-                            text = "Olá Mundo do Kotlin!!",
-                            style = h3,
-                            modifier = Modifier.align(Alignment.Center)
-                        )
-                    }
                 }
             }
         }
     }
 }
 
-
-
-/*Text(
-text = "Olá mundo!!",
-style = MaterialTheme.typography.h3.copy(fontWeight = FontWeight.Bold),
-modifier = Modifier.align(Alignment.Center)
+val h3 = TextStyle(
+    fontWeight = FontWeight.Bold,
+    fontSize = 30.sp,
+    letterSpacing = 0.15.sp
 )
-*/
+@Composable
+fun desenha_tela()
+ {
+     Box(modifier = Modifier.fillMaxSize()) {
+
+         Image(painter = painterResource(id = R.drawable.img1), contentDescription = null, modifier = Modifier.align(Alignment.TopStart).size(100.dp))
+
+         Image(painter = painterResource(id = R.drawable.img2), contentDescription = null, modifier = Modifier.align(Alignment.TopEnd).size(100.dp))
+
+         Image(painter = painterResource(id = R.drawable.img3), contentDescription = null, modifier = Modifier.align(Alignment.BottomStart).size(100.dp))
+
+         Image(painter = painterResource(id = R.drawable.img4), contentDescription = null, modifier = Modifier.align(Alignment.BottomEnd).size(100.dp))
+
+         var texto by remember { mutableStateOf("Olá Mundo do Kotlin!!") }
+
+         Text(
+             text = texto,
+             style = h3,
+             modifier = Modifier.align(Alignment.Center)
+         )
+
+         Button(onClick = { texto = "Hello World" }, modifier = Modifier.align(Alignment.BottomCenter)) {
+             Text(text = "Alterarção")
+         }
+     }
+}
